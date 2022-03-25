3ec6a40f9b1d9ac50d9f7e8c25b2dcaf2641e7c8
jungkihun <khkh0130@gmail.com>
2022-03-26 02:29:31 +0900

    jkh addbutton edit

--- MyTodoList/MyTodoList.js
+++ MyTodoList/MyTodoList.js
@@ -11,21 +11,54 @@ const month = () => {
 }
 const lastDate = () => {
     const lastDate = new Date(year, month, 0);
-    var temp =lastDate.getDate;
+    var temp = lastDate.getDate;
     return temp;
 }
-const addButton = (i) =>  {
-    const addBut = document.getElementById(`todo-add-${i}`);
-    addBut.appendChild(`click`,()=>{
-        let str = `<li>${document.getElementById(`#todoinput`).value}</li>`;
-        document.getElementById(`#todo-list-${i}`).innerHTML = str;
-    });
+const localSave = (str,key) => {
+    localStorage.setItem(key, str);
+}//localsave에서 저장하는 방법
+const localLoad = (key) => {
+    let str = localStorage.getItem(key);
+    if(str == null)
+        str = "";
+    return str;
 }
+const localRemove = (key) =>{
+    localStorage.removeItem(key);
+}
+const addButton = () => {
+var str = "";``
+    for (let i = 1; i < 31; i++) {
+        const addBut = document.getElementById(`todo-add-${i}`);
+        str = localLoad(i);
+        console.log(str);
+        addBut.addEventListener(`click`, () => {
+            let value = document.getElementById(`todoinput-${i}`).value;
+            str +=`<li> ${value} </li>`;
+            localSave(str,i);
+            let k = document.getElementById(`todo-list-${i}`);           
+            k.innerHTML = str;
+        }); 
+    }
+
+}
+const dleButton = () => {
+        for (let i = 1; i < 31; i++) {
+            const delBut = document.getElementById(`todo-delet-${i}`);          
+            delBut.addEventListener(`click`, () => {
+              localRemove(i);
+              let k = document.querySelector(`li`);        
+               k.parentNode.innerHTML = "";
+            }); 
+        }
+    
+    }
+
 const week = () => {
     var str = "<table>";
     console.log(`${lastDate}`);
     const weekKor = (i) => {
-        let otion = i % 7 ;
+        let otion = i % 7;
         var strW;
         switch (otion) {
             case 5:
@@ -54,32 +87,32 @@ const week = () => {
         return strW;
     }
     for (let i = 1; i <= 31; i++) {
-        addButton(i);
+        //addButton(i);
         if (i % 7 == 6) {
             str += `<tr><td id="todo-date">${i}<br><font color=#FF0000>${weekKor(i)}</td>
             <td id="todo-color">&nbsp;</td>
-            <td><input type="text" id="todoinput">
+            <td><input type="text" id="todoinput-${i}">
                 <input type="button" id="todo-add-${i}" value="등록"><br>
                 <input type="button" id="todo-delet-${i}" value="모두삭제"></td>
-            <td><ul id="todo-list-${i}"></ul></td>
+                <td><div id="todo-list-${i}">${localLoad(i)}</div></td>
         </tr>`;
         }//일요일
         else if (i % 7 == 5) {
             str += `<tr><td id="todo-date">${i}<br><font color=#0000FF>${weekKor(i)}</td>
             <td id="todo-color">&nbsp;</td>
-            <td><input type="text" id="todoinput">
+            <td><input type="text" id="todoinput-${i}">
                 <input type="button" id="todo-add-${i}" value="등록"><br>
                 <input type="button" id="todo-delet-${i}" value="모두삭제"></td>
-            <td><ul id="todo-list-${i}"></ul></td>
+            <td><div id="todo-list-${i}">${localLoad(i)}</div></td>
         </tr>`;
         }//토요일
         else {
             str += `<tr><td id="todo-date">${i}<br>${weekKor(i)}</td>
             <td id="todo-color">&nbsp;</td>
-            <td><input type="text" id="todoinput">
+            <td><input type="text" id="todoinput-${i}">
                 <input type="button" id="todo-add-${i}" value="등록"><br>
                 <input type="button" id="todo-delet-${i}" value="모두삭제"></td>
-            <td><ul id="todo-list-${i}"></ul></td>
+                <td><div id="todo-list-${i}">${localLoad(i)}</div></td>
         </tr>`;
         }
     }
@@ -87,8 +120,8 @@ const week = () => {
     console.log(str);
     return str;
 }
-const titleMonth = () =>{
-    var temp =  year() +"-"+month();
+const titleMonth = () => {
+    var temp = year() + "-" + month();
     return temp;
 }
 // var tag = {
@@ -104,3 +137,4 @@ var i = document.getElementById("todo-list");
 var j = document.getElementById("todo-year-month")
 i.innerHTML = week();
 j.innerHTML = titleMonth();
+addButton();
